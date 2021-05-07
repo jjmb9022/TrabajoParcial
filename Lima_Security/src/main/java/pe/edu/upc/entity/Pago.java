c0c37c2f4b8bc519213cfe0115f71ab14e00b7ee
Andreifs <83047973+Andreifs@users.noreply.github.com>
2021-05-06 20:24:48 -0500

    Update Pago.java

--- Lima_Security/src/main/java/pe/edu/upc/entity/Pago.java
+++ Lima_Security/src/main/java/pe/edu/upc/entity/Pago.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "")
+@Table(name = "pago")
 public class Pago implements Serializable {
 
 	private static final long serialVersionUID = 1L;
